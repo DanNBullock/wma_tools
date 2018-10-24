424acc11bd3a97efb42b576e6dceff7a23984f1a
DanNBullock <dnbulloc@umail.iu.edu>
2018-10-24 19:11:24 -0400

    Added multiROIpairSeg

---
+++ bsc_loadAndParseROI.m
@@ -0,0 +1,38 @@
+function [ROI] = bsc_loadAndParseROI(ROIorNifti)
+% [ROI] = bsc_loadAndParseROI(ROIorNifti)
+%
+% Purpose:  Takes in an input, be it a string or an object, loads it if
+% needed and then perform appropriate operations to obtain a vistasoft-relevant roi. 
+%
+% INPUTS
+%
+% -ROIorNifti: either a string or an object, to either a wbFG or an FE
+%  structure
+%
+% OUTPUTS
+%
+% -ROI:  an roi (as if from dtiNewRoi)
+%
+%
+% (C) Daniel Bullock, 2018, Indiana University
+%
+%% Begin code
+
+if  isstring (ROIorNifti)
+    %decomposes file path
+    [fpath,fname,fext]=fileparts(ROIorNifti);
+    if or(strcmp(fext,'.gz'),strcmp(fext,'.nii'))
+        niftiIn=niftiRead(ROIorNifti);
+        ROI=dtiRoiFromNifti(niftiIn,0,'ROI','mat',true,false);
+    else
+        %could cause a problem due to how old vistasoft tends to throw up a
+        %UI prompt in lack of input cases.
+        ROI=dtiReadRoi(ROIorNifti);
+    end
+elseif isstruc(ROIorNifti)
+    if ~isfield(ROIorNifti,'coords')
+        ROI=dtiRoiFromNifti(ROIorNifti,0,'ROI','mat',true,false);
+    elseif isfield(ROIorNifti,'coords')
+        ROI=ROIorNifti;
+    end
+end
---
+++ multiROIpairSeg.m
@@ -0,0 +1,41 @@
+function [classificationOUT]=multiROIpairSeg(feORwbfg,varargin)
+% function [classificationOUT]=multiROIpairSeg(feORwbfg,varargin)
+%
+%  Purpose:  This function segments tracts from an input wbfg by
+%  iteratively segmenting between input ROI pairs.  In theory, input ROIs
+%  can be either .mat format or nifti format, either as strings or as
+%  objects.
+%
+%  Inputs
+%
+%  feORwbfg:  either a whole brain fiber group / tractogram
+%  path/object or an fe path / object.
+%
+%  varargin:  a series of either .mat rois or niftis, either as strings or
+%  as objects.  Inputs are interpreted such that the first input is the
+%  first item  of the pair pairing while the second input is the second
+%  item of the pair.  Thus odds are ROI1 and evens are ROI2
+%
+%  Outputs:
+%
+%  classificationOUT:  A classification structure with names generated
+%  'tract_1' through 'tract_n' where n is the number of ROI pairings
+%  entered.  Future versions can generate names more intelligently.
+%  WARNING: IF A STREAMLINE IS SEGMENTED INTO MULTIPLE TRACTS
+%  IT WILL ONLY BE CLASSIFIED UNDER THE MOST RECENT (LAST) TRACT.
+%
+% %  (C) Daniel Bullock, 2018, Indiana University
+%% begin code
+%load and parse input fg
+[wbFG, ~] = bsc_LoadAndParseFiberStructure(feORwbfg);
+%initialize fg structure
+classificationOUT=[];
+for iPairs=1:length(varargin)/2
+    %obtain and load ROIs
+    roi1= bsc_loadAndParseROI(ROIorNiftivarargin(iPairs*2-1));
+    roi2=bsc_loadAndParseROI(ROIorNiftivarargin(iPairs*2));
+    % do standard segmentation
+    [~, keep]=  bsc_tractByEndpointROIs(wbFG, [roi1,roi2]);
+    %use output boolean to add to classification structure
+    [classificationOUT]=bsc_concatClassificationCriteria(classificationOUT,strcat('tract_',num2str(iPairs),keep));
+end
