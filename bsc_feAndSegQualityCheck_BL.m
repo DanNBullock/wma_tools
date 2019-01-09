a4a78fdf1568337c7b991d7a6afd7ddacdc7149a
Daniel Bullock <dnbulloc@i9.red.uits.iu.edu>
2019-01-09 10:29:32 -0500

    fixed classification issue

--- bsc_feAndSegQualityCheck_BL.m
+++ bsc_feAndSegQualityCheck_BL.m
@@ -7,11 +7,11 @@ function bsc_feAndSegQualityCheck_BL()
 
  if ~isdeployed
     disp('adding paths');
+     addpath(genpath('/N/u/brlife/git/encode'))
      addpath(genpath('/N/soft/rhel7/spm/8')) %spm needs to be loaded before vistasoft as vistasoft provides anmean that works
      addpath(genpath('/N/u/brlife/git/jsonlab'))
      addpath(genpath('/N/u/brlife/git/vistasoft'))
      addpath(genpath('/N/u/brlife/git/wma_tools'))
-     addpath(genpath('/N/u/brlife/git/encode'))
  end
 
 %config = loadjson('/N/dc2/projects/lifebid/HCP/Dan/GitStoreDir/ROIs2ROIsSegment/config.json');
@@ -23,13 +23,13 @@ else
     feORwbfg=config.wbfg;
 end
 
-if isfield(config,'classification')
-    load(config.classification)
-      classification=classification;
-end
-
 saveDir=pwd;
 
-bsc_feAndSegQualityCheck(feORwbfg, classification, saveDir)
-
+if isfield(config,'classification')
+    load(config.classification)
+    classification=classification;
+    bsc_feAndSegQualityCheck(feORwbfg, classification, saveDir)
+else
+    bsc_feAndSegQualityCheck(feORwbfg, [], saveDir)
+end
 end
