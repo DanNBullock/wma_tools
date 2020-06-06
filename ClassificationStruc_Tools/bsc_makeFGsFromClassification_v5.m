da4b0284de7ef364512b65e6008b0fb995f06e20
Daniel Bullock <dnbulloc@iu.edu>
2020-06-06 13:51:32 -0400

    updated fg from classificaiton function New features: assigns appropriate color groups to fgs in HSV map imposes RAS LPI orientation on output tracts

--- ClassificationStruc_Tools/bsc_makeFGsFromClassification_v5.m
+++ ClassificationStruc_Tools/bsc_makeFGsFromClassification_v5.m
@@ -19,10 +19,11 @@ function tractStruc = bsc_makeFGsFromClassification_v5(classification, wbfg)
 %   depending on your version of vistasoft.  Will load paths.
 %
 %
-% OUTPUTS 
-% tractStruc:  structure wherein each row corresponds to a diffrent fg.
-% Previous version added a layer of complexity to this.  Now we are working
-% acorss fields?
+% OUTPUTS tractStruc:  structure wherein each row corresponds to a diffrent
+% fg. Previous version added a layer of complexity to this, such there were
+% fg and name fields.  This was redundant because there was a .name field
+% within .fg.  As such, this is now just a cell structure (wherein each
+% cell contains a fg object), and thus there are no fields.
 %
 %
 % (C) Daniel Bullock, 2017, Indiana University
@@ -31,7 +32,7 @@ function tractStruc = bsc_makeFGsFromClassification_v5(classification, wbfg)
 
 % loads object if path passed
 if ischar(wbfg)
-wbfg = fgRead(wbfg);
+    wbfg = fgRead(wbfg);
 else
     %do nothing
 end
@@ -43,12 +44,13 @@ else
     %do nothing
 end
 
-% just for asthetic purposes, complexify this later with groupings if you
-% want
-colorMapping = rand(length(classification.names),3);
+% just for asthetic purposes, selects colors and applies to them to the
+% tract
+classificationGrouping = wma_classificationStrucGrouping(classification);
+colorMapping = colormap(hsv(max(classificationGrouping.index)));
 % end
 
-if isempty(classification.names) 
+if isempty(classification.names)
     error('classification.names is empty.. something went wrong?');
 end
 
@@ -60,12 +62,14 @@ end
 for itracts=1:length(classification.names)
     %use dti toolset to make new holder with specified name
     tractStruc{itracts} = dtiNewFiberGroup(classification.names{itracts});
-    %Set color
-    tractStruc{itracts}.colorRgb=colorMapping(itracts,:);
+    %Set color, a bit more complicated than you'd expect
+    currentColor=colorMapping(unique(classificationGrouping.index(classification.index==itracts)),:);
+    tractStruc{itracts}.colorRgb=currentColor;
     %preface the creation of tract with feedback
     fprintf('\n creating tract for %s with %i streamlines', classification.names{itracts},sum(classification.index==itracts) );
     %assign streams to new fg object
     tractStruc{itracts}.fibers=wbfg.fibers(classification.index==itracts);
+    %impose orientation on it
+    tractStruc{itracts}=bsc_orientFgRAS_LPI(tractStruc{itracts});
 end
-
 end
